20d7cd9aa7bb848bc630879964f93a78b289371d
atoiman <fra_bridon@hotmail.fr>
2018-10-09 00:12:09 +0200

    ft_strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,46 +1,28 @@
-#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 
-int	ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strstr(char *str,char *to_find)
+char	*ft_strstr(const char *str, const char *to_find)
 {
 	int i;
 	int j;
-	int k;
 
 	i = 0;
-	j= 0;
-	k = ft_strlen(to_find);
-	while(str != to_find)
-	{
-		str++;
-		i++;
-	}
-	while (str[i] == to_find[j])
+	j = 0;
+	while (str[i])
 	{
 		j = 0;
-		while (str[i+j] == to_find[j])
-				j++;
-		if (j == k)
-		return (&str[i]);
+		while (str[i + j] == to_find[j])
+		{
+			if(to_find[j + 1] == '\0')
+				return (str + i);
+			j++;
+		}
+		i++;
 	}
-	return (0);
 }
 
-
-int main(int ac, char **av)
+int	main(int ac , char **av)
 {
-	char str[] = "salut les copaings";
-	char to_find[] = "les";
-	printf("%s\n",ft_strstr(str , to_find));
-	return  (0);
+	printf("%s\n", ft_strstr(av[1], av[2]));
+	return (0);
 }
